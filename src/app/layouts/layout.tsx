107984250b91a003a3c69ffdbbd3d070a1f094b2
k1ngsterr1 <ruslanmakhmatov@gmail.com>
2024-11-07 20:10:26 +0500

    feat: finish survey screen

--- src/app/layouts/layout.tsx
+++ src/app/layouts/layout.tsx
@@ -31,14 +31,16 @@ export const Layout: React.FC<ILayout> = ({
         contentContainerStyle={{ paddingBottom: isTab ? 80 : 0 }}
         className="flex-1 bg-white"
       >
-        <View className="w-full items-center justify-center mt-12">
+        <View className="w-full flex items-center justify-center mt-12">
           {isLogo && (
             <Text className="text-dark text-xl">
               <Text className="text-main">Eco</Text>Habits
             </Text>
           )}
         </View>
-        <View className="flex-1 px-4">{children}</View>
+        <View className="flex flex-col items-center justify-center  px-4">
+          {children}
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
--- src/app/screens/SurveyScreen/survey-screen.tsx
+++ src/app/screens/SurveyScreen/survey-screen.tsx
@@ -9,15 +9,20 @@ import TreeIcon from "@shared/ui/Icons/tree-icon";
 export const SurveyScreen = () => {
   return (
     <Layout>
-      <Text className="text-4xl text-dark font-bold">
-        Help Us Tailor Your
-        <Text className="text-main font-bold">Eco Plan!</Text>
-      </Text>
-      <View className="w-[280px] h-[200px] border-main border-[1px] rounded-lg flex flex-col items-center justify-center">
-        <TreeIcon />
-        <Text className="text-base text-dark text-center">
-          Let's start by learning more about your habit
+      <View className="h-[80vh] relative flex items-center justify-center">
+        <Text className="text-3xl text-dark text-center font-bold w-[280px]">
+          Help Us Tailor Your
+          <Text className="text-main font-bold"> Eco Plan!</Text>
         </Text>
+        <View className="w-[280px] h-[200px] border-main border-[1px] rounded-2xl flex flex-col items-center justify-center mt-8">
+          <TreeIcon />
+          <Text className="text-base text-dark text-center">
+            Let's start by learning more about your habit
+          </Text>
+        </View>
+        <MyTouchableOpacity className="w-[270px] m-auto absolute bottom-4 h-[50px] bg-main flex items-center justify-center rounded-full">
+          <Text className="text-white">Take the Survey</Text>
+        </MyTouchableOpacity>
       </View>
     </Layout>
   );
